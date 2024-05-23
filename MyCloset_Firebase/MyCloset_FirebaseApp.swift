6a76a430287d421d66d6c182536a9cc99683136b
Muniseozen <munisebusiness@gmail.com>
2024-05-23 14:47:44 +0900

    Initial Commit

---
+++ MyCloset_Firebase/MyCloset_FirebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyCloset_FirebaseApp.swift
+//  MyCloset_Firebase
+//
+//  Created by Özen Munise on 2024/05/23.
+//
+
+import SwiftUI
+
+@main
+struct MyCloset_FirebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
